f1d278ff3bdc9c9b1caa66037a4aa2d249c1d835
Vysakhchandren <vysakhc14@gmail.com>
2025-09-25 15:47:39 +0530

    Search for books from json file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
+import 'dart:developer';
+
+import 'package:book_reader/network/network.dart';
 import 'package:flutter/material.dart';
+
+import 'models/book.dart';
+import 'pages/favorite_screen.dart';
+import 'pages/home_screen.dart';
+import 'pages/saved_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,55 +22,73 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
       ),
       home: const MyHomePage(),
     );
   }
 }
- 
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
-
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
-  @override
 
+  Network network = Network();
+
+  Future<void> _searchBooks(String query) async {
+    try {
+      List<Book> books= await network.searchBooks(query);
+      print("Books: ${books.toString()}");
+    } catch (e) {}
+  }
+
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const SavedScreen(),
+    const FavoriteScreen(),
+  ];
+
+  @override
+  void initState() {
+    _searchBooks('Android');
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('A.Reader'),
       ),
-      body: const Column(
-        children: [Text("Hello")],
-      ),
+      body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.save),label: 'Save'),
-        BottomNavigationBarItem(icon: Icon(Icons.favorite),label: 'Favorite')
-
-      ],
-      selectedItemColor: Theme.of(context).colorScheme.onPrimary,
-      unselectedItemColor:Theme.of(context).colorScheme.onPrimary,
-      onTap: (value) {
-          print('Tapped: $value');
-        setState(() {
-          _currentIndex = value;
-        });
-        print('Tapped CurrentIndex: $_currentIndex');
-
-      },
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.save), label: 'Save'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+        ],
+        selectedItemColor: Theme.of(context).colorScheme.onPrimary,
+        unselectedItemColor: Theme.of(context).colorScheme.onSurfaceVariant,
+        onTap: (value) {
+          log('Tapped: $value');
+          setState(() {
+            _currentIndex = value;
+          });
+          log('Tapped CurrentIndex: $_currentIndex');
+        },
       ),
-
     );
   }
 }
