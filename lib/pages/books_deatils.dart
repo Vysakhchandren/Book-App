7b624569b5ccc5b500868f91f957f8f15b6f51e6
Vysakhchandren <vysakhc14@gmail.com>
2025-10-16 00:04:48 +0530

    Add insert and readAllbooks

--- lib/pages/books_deatils.dart
+++ lib/pages/books_deatils.dart
@@ -13,41 +13,70 @@ class BooksDetailsScreen extends StatefulWidget {
 class _BooksDetailsScreenState extends State<BooksDetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    final args = ModalRoute.of(context)?.settings.arguments as BookDetailsArguments;
+    final args =
+        ModalRoute.of(context)?.settings.arguments as BookDetailsArguments;
     final Book book = args.itemBook;
     final theme = Theme.of(context).textTheme;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(book.title),
-      ),
+      appBar: AppBar(title: Text(book.title)),
       body: SingleChildScrollView(
         child: Center(
           child: Column(
             children: [
-              if(book.imageLinks.isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.all(18.0),
-              child: Image.network(
-                book.imageLinks["thumbnail"] ?? '',
-                fit: BoxFit.cover,
-              ),
-              ),
+              if (book.imageLinks.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Image.network(
+                    book.imageLinks["thumbnail"] ?? '',
+                    fit: BoxFit.cover,
+                  ),
+                ),
               Column(
                 children: [
+                  Text(book.title, style: theme.headlineSmall),
+                  Text(book.authors.join(', '), style: theme.labelLarge),
                   Text(
-                    book.title,
-                    style: theme.headlineSmall,
+                    'Publised: ${book.publishedDate}',
+                    style: theme.bodySmall,
                   ),
-                  Text(book.authors.join(', '),
-                  style: theme.labelLarge,),
-                  Text('Publised: ${book.publishedDate}',
-                  style: theme.bodySmall,),
-                  Text('Page count: ${book.pageCount}',
-                  style: theme.bodySmall,),
-                  Text('Language :${book.language}',
-                  style: theme.bodySmall,)
+                  Text('Page count: ${book.pageCount}', style: theme.bodySmall),
+                  Text('Language :${book.language}', style: theme.bodySmall),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(onPressed: () {}, child: Text('Save')),
+                      ElevatedButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.favorite),
+                        label: Text('Favorite'),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Text('Description', style: theme.titleMedium),
+                  SizedBox(height: 5),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.secondary.withValues(
+                        blue: .4,
+                        green: .7,
+                        alpha: .3,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    child: Text(
+                      book.description,
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
