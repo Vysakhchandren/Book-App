bd9d87bb109795da56da6bd70f51c26366f9ba94
Vysakhchandren <vysakhc14@gmail.com>
2025-10-16 23:17:29 +0530

    Show saved book in the save screen

--- lib/pages/saved_screen.dart
+++ lib/pages/saved_screen.dart
@@ -1,4 +1,7 @@
+import 'package:book_reader/db/database_helper.dart';
 import 'package:flutter/material.dart';
+
+import '../models/book.dart';
 
 class SavedScreen extends StatefulWidget {
   const SavedScreen({super.key});
@@ -11,8 +14,16 @@ class _SavedScreenState extends State<SavedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Saved'),
+      appBar: AppBar(title: const Text('Saved')),
+      body: FutureBuilder(
+        future: DatabaseHelper.instance.readAllBooks(),
+        builder: (context, snapshot) => snapshot.hasData
+            ? ListView.builder(itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              Book book = snapshot.data![index];
+              return ListTile(title: Text(book.title),);
+        })
+            : const Center(child: const CircularProgressIndicator()),
       ),
     );
   }
